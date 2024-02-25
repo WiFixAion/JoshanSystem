baacc883a9540eb637eff08b1895801d9210485b
WiFixAion <161218080+WiFixAion@users.noreply.github.com>
2024-02-25 21:08:38 +0800

    Update login.php

--- framework/login.php
+++ framework/login.php
@@ -30,23 +30,26 @@ if(isset($_REQUEST['submit'])){
 <link href="https://fonts.googleapis.com/css2?family=Assistant&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/custom.css?<?php echo time();?>">
 </head>
-<body>
-<div id="brand-block">
-    <h2>Name of Company</h2>
-</div>
-<div id="login-block">
-	<h3>Please login</h3>
-	<form method="POST" action="" name="login">
-	<div>
-		<input type="email" class="input" required name="useremail" placeholder="Valid E-mail"/>
-	</div>
-	<div>
-		<input type="password" class="input" required name="password" placeholder="Password"/>
-	</div>
-	<div>
-		<input type="submit" name="submit" value="Submit"/>
-	</div>
-	</form>
+<body style="background-image: url(https://t3.ftcdn.net/jpg/05/79/48/50/360_F_579485091_aVxVKR8e2s886hee1j146OBeiJugJifG.jpg);">
+<div class="container">
+    <div class="brand-block">
+        <h2>HUMBLEZ</h2>
+    </div>
+    <div class="login-block">
+        <h3>Please login</h3>
+        <form method="POST" action="" name="login">
+            <div class="form-group">
+                <input type="email" class="input" required name="useremail" placeholder="Valid E-mail"/>
+            </div>
+            <div class="form-group">
+                <input type="password" class="input" required name="password" placeholder="Password"/>
+            </div>
+            <div class="form-group">
+                <input type="submit" name="submit" value="Submit" class="btn"/>
+            </div>
+        </form>
+        <div id='error_notif' class="error-message">Wrong email or password.</div>
+    </div>
 </div>
 </body>
 </html>
